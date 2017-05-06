eaeff105d6b5655fcd13c820890cf38c6e7774bd
zcrez <121887124@qq.com>
2017-05-07 01:55:58 +0800

    5-7

---
+++ MyMarkers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CustomMarkers
+{
+    using System;
+    using System.Drawing;
+    using System.Runtime.Serialization;
+    using GMap.NET;
+    using GMap.NET.WindowsForms;
+    class MyMarkers 
+    {
+        public  MyMarkers()
+        {
+            return;
+        }
+    }
+}
---
+++ common.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PosCheckApp
+{
+    class PosPT
+    {
+        public string ID;
+        public string photoID;
+        public double lat;
+        public double lng;
+        public double alt;
+        public double pitch;
+        public double head;
+        public double roll;
+        public bool isValid;
+
+        public PosPT()
+        {
+            this.isValid = false;
+        }
+    }
+}
